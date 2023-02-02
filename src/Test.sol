4a3c041ce7173a5c7f512162468d0d5b872c394e
Ladislav Dubravský <dubravsky90@gmail.com>
2023-02-02 21:40:25 +0100

    Initial commit

---
+++ src/Test.sol
@@ -0,0 +1,8 @@
+//SPDX-License-Identifier: MIT
+pragma solidity 0.8.17;
+
+import "./Types.sol";
+
+contract Test is Types {
+    TokenType public abc;
+}
---
+++ src/Types.sol
@@ -0,0 +1,10 @@
+//SPDX-License-Identifier: MIT
+pragma solidity 0.8.17;
+
+contract Types {
+    enum TokenType {
+        ERC20,
+        ERC721,
+        ERC1155
+    }
+}
